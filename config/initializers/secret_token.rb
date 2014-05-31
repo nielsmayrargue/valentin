eda3fc2bafad741fdc119b69ce5274e72f720318
Niels Mayrargue <niels.mayrargue@gmail.com>
2014-05-31 18:03:15 +0200

    app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Valentin::Application.config.secret_key_base = '455d17892376b2d3b9ad0904c9d5cc53a5e3b1c389ad56666f08a81a5452ba52ff3b6a5b95a6c4a06a4f177a760b4e1b5c3a6f40f404119fa478c37ce58a7b00'
